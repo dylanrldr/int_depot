68855932686d38faab018fc39d2623e84e24c279
User1 <riouledrogo.d@stjolorient.fr>
2025-01-13 11:31:26 +0100

    ajout lignes

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -2,7 +2,8 @@
 
 def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
-    return True
+    with open(file_first) as f1, open(file_second) as f2:
+    return f1.read() != f2.read()
 
 def same(file_first, file_second):
     """Fonction retournant True si deux fichiers sont identiques."""
